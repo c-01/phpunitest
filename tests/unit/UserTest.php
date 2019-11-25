a9e37f0c3ff52a7ba4edc4054dbcc5d1f329f5d4
Khung Long Bao Chua <emailcuamayban@gmail.com>
2019-11-25 09:42:08 +0700

    push

---
+++ tests/unit/UserTest.php
@@ -0,0 +1,15 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class UserTest extends TestCase
+{
+
+    public function testWeCanGetFirstName()
+    {
+        // create User
+        $user = new \App\Models\User;
+        // set First Name
+        // test get first name
+    }
+}
