c1d49f7bb8a39cfe46a51e3ab0b92e1c1218df4a
hideMan <thanhdntage@gmail.com>
2019-11-25 11:06:11 +0700

    test User model

--- tests/unit/UserTest.php
+++ tests/unit/UserTest.php
@@ -1,6 +1,7 @@
 <?php
 
 use PHPUnit\Framework\TestCase;
+use App\Models\User;
 
 class UserTest extends TestCase
 {
@@ -8,8 +9,48 @@ class UserTest extends TestCase
     public function testWeCanGetFirstName()
     {
         // create User
-        $user = new \App\Models\User;
+        $user = new User;
         // set First Name
+        $_firstName = 'Hooman';
+
+        $user->setFirstName($_firstName);
         // test get first name
+        $this->assertEquals($user->getFirstName(),$_firstName);
+    }
+
+    public function testWeCanGetLastName()
+    {
+        $user = new User;
+        $_lastName = 'Doe';
+        $user->setLastName($_lastName);
+        $this->assertEquals($user->getLastName(),$_lastName);
+    }
+
+    public function testFullNameIsReturned()
+    {
+        $user = new User;
+        $_firstName = 'Hooman';
+        $_lastName = 'Doe';
+
+        $user->setFirstName($_firstName);
+        $user->setLastName($_lastName);
+
+        $this->assertEquals($user->getFullName(), sprintf('%s %s',$_firstName, $_lastName));
+
+
+    }
+
+    public function testFirstAndLastNameIsTrimmed()
+    {
+        $user = new User;
+        $_firstName = ' Hooman';
+        $_lastName = 'Doe ';
+
+        $user->setFirstName($_firstName);
+        $user->setLastName($_lastName);
+
+        $this->assertEquals($user->getFirstName(), 'Hooman');
+        $this->assertEquals($user->getLastName(), 'Doe');
+
     }
 }
